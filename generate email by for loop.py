844636eced3c0cb108e84df48173e0032d6921bd
mannepalli uday kumar <65896248+uk8380@users.noreply.github.com>
2021-06-19 13:44:52 +0530

    Update generate email by for loop.py

--- generate email by for loop.py
+++ generate email by for loop.py
@@ -1,5 +1,5 @@
 for i in range(1,11):
-    print("UDERNAME: ","uk_",i)#username
+    print("USERNAME: ","uk_",i)#username
     print("GMAIL: ","uk",i,"@gmail.com")#gmail
     print("HOTMAIL: ","ak",i,"@hotmail.in")#hotmail
     print("-------------------------------")
